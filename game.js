e492f4311fdffcba5e64de0d4e12b0ef6b1385cf
James Zhong <thebitspud@gmail.com>
2019-01-27 06:04:27 -0800

    nunu ouroboros

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ const config = {
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 500 },
+			gravity: { y: 600 },
 			debug: false
 		}
 	},
@@ -22,36 +22,28 @@ function preload() {
 	this.load.spritesheet('ningen', 'assets/ningen.png', { frameWidth: 42, frameHeight: 64 });
 	this.load.image('bg-ice', 'assets/bg-ice.png');
 	this.load.image('platform', 'assets/platform.png');
+	this.load.image('ground', 'assets/ground.png');
 	this.load.image('nunu', 'assets/nunu.png');
 	this.load.image('bomb', 'assets/bomb.png');
+	this.load.image('axe', 'assets/axe.png');
 }
 
-var ningen, platforms, nunus, bombs, cursors, keys, lastDir, score = 0, gameOver = false, scoreText;
+var ningen, platforms, nunus, bombs,
+	cursors, spacebar, lastDir,
+	nextLevel = 0, score = 0, collected = 0,
+	gameOver = false,
+	scoreText;
 
 function create() {
 	this.add.image(0, 0, 'bg-ice').setOrigin(0, 0);
 
 	platforms = this.physics.add.staticGroup();
 
-	platforms.create(60, 584, 'platform');
-	platforms.create(180, 584, 'platform');
-	platforms.create(300, 584, 'platform');
-	platforms.create(420, 584, 'platform');
-	platforms.create(540, 584, 'platform');
-	platforms.create(660, 584, 'platform');
-	platforms.create(780, 584, 'platform');
-
-	platforms.create(600, 400, 'platform');
-	platforms.create(50, 250, 'platform');
-	platforms.create(230, 300, 'platform');
-	platforms.create(750, 220, 'platform');
-	platforms.create(400, 410, 'platform');
-	platforms.create(440, 150, 'platform');
-	
-	ningen = this.physics.add.sprite(300, 535, 'ningen');
-
+	ningen = this.physics.add.sprite(400, 520, 'ningen');
 	ningen.setCollideWorldBounds(true);
 	this.physics.add.collider(ningen, platforms);
+
+	loadStage(nextLevel);
 
 	this.anims.create({
 		key: 'left',
@@ -80,11 +72,12 @@ function create() {
 	});
 
 	cursors = this.input.keyboard.createCursorKeys();
+	spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
 	nunus = this.physics.add.group({
 		key: 'nunu',
-		repeat: 10,
-		setXY: { x: 50, y: 0, stepX: 70 }
+		repeat: 9,
+		setXY: { x: 60, y: 0, stepX: 75 }
 	});
 	
 	nunus.children.iterate(function (child) {
@@ -104,21 +97,80 @@ function create() {
 	this.physics.add.collider(ningen, bombs, hitBomb, null, this);
 }
 
+function loadStage(level) {
+	platforms.children.iterate(function (child) {
+		child.disableBody(true, true);
+	});
+	platforms.clear();
+	platforms.create(400, 575, 'ground');
+	createPlatforms(level);
+	nextLevel++;
+}
+
+function createPlatforms(level) {
+	switch(level) {
+		case 0: break;
+
+		case 1:
+		platforms.create(150, 250, 'platform');
+		platforms.create(650, 250, 'platform');
+		platforms.create(400, 400, 'platform');
+		break;
+
+		case 2:
+		platforms.create(600, 400, 'platform');
+		platforms.create(230, 300, 'platform');
+		platforms.create(750, 220, 'platform');
+		platforms.create(400, 410, 'platform');
+		platforms.create(150, 150, 'platform');
+		break;
+
+		case 3:
+		default:
+		platforms.create(600, 400, 'platform');
+		platforms.create(250, 450, 'platform');
+		platforms.create(750, 400, 'platform');
+		platforms.create(500, 250, 'platform');
+		platforms.create(420, 100, 'platform');
+		platforms.create(50, 330, 'platform');
+		platforms.create(250, 300, 'platform');
+		platforms.create(140, 120, 'platform');
+		break;
+	}
+}
+
+function resetLevel() {
+	score -= (collected * 10);
+	collected = 0;
+	scoreText.setText('Score: ' + score);
+
+	nunus.children.iterate(function (child) {
+		child.enableBody(true, child.x, 0, true, true);
+	});
+
+	ningen.setTint(0xffffff);
+	gameOver = false;
+}
+
 function update() {
+	cursors = this.input.keyboard.createCursorKeys();
+
 	if(gameOver) {
+		if(spacebar.isDown) {
+			resetLevel();
+		}
+		
 		return;
 	}
 
-	cursors = this.input.keyboard.createCursorKeys();
-
 	if (cursors.left.isDown) {
-		ningen.setVelocityX(-180);
+		ningen.setVelocityX(-200);
 
 		if(ningen.body.touching.down) ningen.anims.play('left', true);
 		else ningen.anims.play('still-left', true);
 		lastDir = 'left';
 	} else if (cursors.right.isDown) {
-		ningen.setVelocityX(180);
+		ningen.setVelocityX(200);
 
 		if(ningen.body.touching.down) ningen.anims.play('right', true);
 		else ningen.anims.play('still-right', true);
@@ -142,28 +194,36 @@ function collectNunu (ningen, nunu) {
 	nunu.disableBody(true, true);
 
 	score += 10;
+	collected++;
 	scoreText.setText('Score: ' + score);
 
 	if (nunus.countActive(true) === 0) {
+		collected = 0;
 		nunus.children.iterate(function (child) {
 			child.enableBody(true, child.x, 0, true, true);
 		});
 
-		var x = (ningen.x < 400) ? Phaser.Math.Between(600, 800) : Phaser.Math.Between(0, 200);
+		loadStage(nextLevel);
+		spawnBomb();
+	}
+}
 
-		var bomb = bombs.create(x, 16, 'bomb');
+function spawnBomb() {
+	var x = (ningen.x < 400) ? Phaser.Math.Between(600, 800) : Phaser.Math.Between(0, 200);
+	var vx = Math.random() < 0.5 ? Phaser.Math.Between(-100, -150) : Phaser.Math.Between(100, 150);
+
+	var bomb = bombs.create(x, 16, 'bomb');
 		bomb.setBounce(1);
 		bomb.setCollideWorldBounds(true);
-		bomb.setVelocity(Phaser.Math.Between(-100, 100), 10);
-		bomb.allowGravity = false;
-	}
+		bomb.setVelocity(vx, 5);;
 }
 
 function hitBomb (ningen, bomb) {
 	bomb.disableBody(true, true);
-	this.physics.pause();
 	ningen.anims.play('still-right');
+	ningen.setVelocity(0, 0);
 
 	ningen.setTint(0xff0000);
 	gameOver = true;
+	spawnBomb();
 }
